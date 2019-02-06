bb4e00cfbf92ab91a1776acf7d10c1b4b9156886
Ekaterina <zireael6@gmail.com>
2019-02-07 02:08:24 +0300

    добавлены значения для каждого пикера в настройках

--- Alkotester/SettingsVC.swift
+++ Alkotester/SettingsVC.swift
@@ -8,23 +8,91 @@
 
 import UIKit
 
-class SettingsVC: UIViewController {
-
-    override func viewDidLoad() {
+class SettingsVC: UIViewController
+{
+    @IBOutlet weak var genderPicker: UIPickerView!
+    @IBOutlet weak var heightPicker: UIPickerView!
+    @IBOutlet weak var weightPicker: UIPickerView!
+    var genderPickerData = ["Мужской", "Женский"]
+    var heightPickerData: [String] = []
+    var weightPickerData: [String] = []
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        initializePickers()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Инициализация Picker View's
+    
+    func initializePickers()
+    {
+        genderPicker.delegate = self
+        genderPicker.dataSource = self
+        setHeightPickerData()
+        heightPicker.delegate = self
+        heightPicker.dataSource = self
+        setWeightPickerData()
+        weightPicker.delegate = self
+        weightPicker.dataSource = self
     }
-    */
+    
+    func setHeightPickerData()
+    {
+        for i in 140...210
+        {
+            heightPickerData.append(String(i))
+        }
+    }
+    
+    func setWeightPickerData()
+    {
+        for i in 30...200
+        {
+            weightPickerData.append(String(i))
+        }
+    }
 
+    
+    // MARK: - Расширение
+
+}
+
+extension SettingsVC: UIPickerViewDelegate, UIPickerViewDataSource
+{
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        switch pickerView
+        {
+        case genderPicker:
+            return genderPickerData.count
+        case heightPicker:
+            return heightPickerData.count
+        case weightPicker:
+            return weightPickerData.count
+        default:
+            return 0
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        switch pickerView
+        {
+        case genderPicker:
+            return genderPickerData[row]
+        case heightPicker:
+            return heightPickerData[row]
+        case weightPicker:
+            return weightPickerData[row]
+        default:
+            return "0"
+        }
+    }
 }
