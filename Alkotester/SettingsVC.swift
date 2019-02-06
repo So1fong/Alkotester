4485c3038cb260d60add8db17f1127674005fc69
Ekaterina <zireael6@gmail.com>
2019-02-06 23:23:28 +0300

    добавлены классы для всех VC

---
+++ Alkotester/SettingsVC.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsVC.swift
+//  Alkotester
+//
+//  Created by Ekaterina Kozlova on 06.02.2019.
+//  Copyright © 2019 Ekaterina Kozlova. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
