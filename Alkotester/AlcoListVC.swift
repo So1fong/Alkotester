9907783ae1e8bc8c66fc32275dd61b61fb646544
Ekaterina <zireael6@gmail.com>
2019-02-09 01:13:09 +0300

    добавлена таблица алкогольных напитков

--- Alkotester/AlcoListVC.swift
+++ Alkotester/AlcoListVC.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class AlcoListVC: UIViewController {
+class AlcoListVC: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return alcoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = alcoArray[indexPath.row]
+        return cell
+    }
+    
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    var alcoArray = ["Пиво", "Шампанское", "Вино", "Водка", "Виски", "Ром", "Джин", "Коньяк", "Ликер", "Сакэ", "Бренди", "Абсент", "Самбука", "Текила", "Мартини", "Настойка", "Наливка", "Алкогольный коктейль", "Сидр", "Эль"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
         //navigationItem.leftBarButtonIte
         //navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelTapped))
     }
